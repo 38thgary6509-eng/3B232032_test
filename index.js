4adb774fd1175b2f6681760fbc6b5fd97831145e
38thgary6509-eng <3b232032@gm.student.ncut.edu.tw>
2025-12-12 12:10:06 +0800

    JavaScript

---
+++ index.js
@@ -0,0 +1,5 @@
+document.getElementById("txt").textContent = "您好";
+const p = document.createElement("p");
+p.textContent = "動態文字段落";
+document.body.appendChild(p);
+document.getElementById("heading").innerHTML = "<h1>標題</h1>";
---
+++ oo.js
@@ -0,0 +1,2 @@
+var user = { name: 'yu', age: '21' };
+document.getElementById("user").innerHTML = user.name + "<br>" + user.age + "歲<br>";
