31442f16212cf30874a42c3ca2a1a555d7eff19f
38thgary6509-eng <3b232032@gm.student.ncut.edu.tw>
2025-12-19 12:02:25 +0800

    1219

--- oo.js
+++ oo.js
@@ -1,2 +1,22 @@
-var user = { name: 'yu', age: '21' };
+var user = {
+    name: 'yu', age: '21',
+    showMsg: function () {
+        alert("Hi,我是" + this.name + ",今年" + this.age + "歲")
+    }
+};
+
+//user.name = "某某某";
+//user.age = "10";
+
+document.getElementById("name").placeholder = user.name;
+document.getElementById("age").placeholder = user.age;
 document.getElementById("user").innerHTML = user.name + "<br>" + user.age + "歲<br>";
+
+// alert(user.name+user.age);
+
+var score = {
+    chinese: '90', english: '95', math: '100',
+    showMsg: function () {
+        alert("國問" + this.chinese + "、英文" + this.english + "、數學" + this.math);
+    }
+};
