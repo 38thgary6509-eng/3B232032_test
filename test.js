445bc53ecedb20661eb0c7d57b3233c4b87dc5de
38thgary6509-eng <3b232032@gm.student.ncut.edu.tw>
2025-12-24 21:27:08 +0800

    1224

--- test.js
+++ test.js
@@ -10,26 +10,27 @@ alert(sum(10, 20));
 //自定義function
 //將資料存放於Console(開發人員主控台)裡面，Log紀錄
 //Chrome瀏覽器 -> F12 ->Console
-// function greet() {
-//     const name = document.getElementById("UserName").value;
-//     console.log(name);
-// }//結尾沒有分號
+function greet() {
+    const name = document.getElementById("userName").value;
+    console.log(name);
+}//結尾沒有分號
 
-// //標準function製作
-// function greetUser() {
-//     const name = document.getElementById("UserName").value;
-//     document.getElementById("resultUser").textContent = name + "您好！";
-// }
+//標準function製作
+function greetUser() {
+    const name = document.getElementById("userName").value;
+    document.getElementById("resultUser").textContent = name + "安安您好!";
+}
 
 //模組化function：函式分工、模組化設計、利於擴充
 function getUserName() {
-    const name = document.getElementById("UserName").value;
+    const name = document.getElementById("userName").value;
     //......
     //......
     //......
     //呼叫執行事件函式
     showMessage(name);
 }
+
 function showMessage(name) {
-    document.getElementById("resultUser").textContent = name + "您好！";
+    document.getElementById("resultUser").textContent = name + "安安您好!";
 }
